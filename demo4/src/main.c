d4f63fad769c764b5f6a24e074f44dfb20c82a9e
yangjie <2534321244@qq.com>
2021-05-06 11:16:41 +0800

    添加cmake学习过程中的demo1到demo3

--- demo4/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "hello.h"
-
-int main()
-{
-    hello("I'm demo4.");
-    return 0;
-}
--- demo5/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "hello.h"
-
-int main()
-{
-    hello("I'm demo5.");
-    return 0;
-}
